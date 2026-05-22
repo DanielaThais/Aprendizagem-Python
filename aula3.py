77fba5c4410ab64a13cf80330559e4e8c051d7be
DanielaThais <daniela.thais64@gmail.com>
2026-05-22 13:21:16 -0300

    tipo String

---
+++ aula3.py
@@ -0,0 +1,24 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem=  Dinâmica / Forte
+str = string -: texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas Simples
+print('Daniela Thais')
+
+# Aspas Duplas
+print("Daniela Thais")
+
+# Escape
+print("Daniela \"Thais\"")
+
+# r
+print(r"Daniela \"Thais\"") #usado para expressões regulares
+
+# A forma mais limpa e simples de fazer é assim:
+
+print('Daniela "Thais"')
+print("Daniela 'Thais'")
