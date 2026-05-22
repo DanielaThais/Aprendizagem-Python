9ee1bf1cfdc17ef8c50acbe1075ba24e7692bdc0
DanielaThais <daniela.thais64@gmail.com>
2026-05-22 13:00:17 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,9 @@
+# comentário em Python para exemplificar que só funciona em linha única
+print("Hello, World!")
+
+"""
+Isso não é um comentário, pois o Python não ignora ela
+
+
+Porém, ela é uma string que pode ser utilizada para documentação de várias linhas.
+"""
