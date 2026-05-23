c73769e2c691e7e87f549a252db74812fa54bb7f
DanielaThais <daniela.thais64@gmail.com>
2026-05-23 10:28:35 -0300

    Resolução do exercício de variáveis

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'Daniela'
+sobrenome = 'Thais'
+idade = 23
+ano_nascimento = 2002
+maior_idade = idade >= 18
+altura = 1.60
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('Maior de idade? ', maior_idade)
+print('Altura: ', altura)
