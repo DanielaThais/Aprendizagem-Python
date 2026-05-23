79c252ade9e460744b2c3e20e74ecf1be366e115
DanielaThais <daniela.thais64@gmail.com>
2026-05-23 10:51:16 -0300

    exercício cálculo de IMC

---
+++ aula12.py
@@ -0,0 +1,14 @@
+# EXERCÍCIO DE CÁLCULO D E IMC
+
+# ENUNCIADO: Daniela tem 1.60 de altura, pesa 65 quilos e seu IMC é 25.39.
+
+nome = 'Daniela Thais'
+altura = 1.60
+peso = 65
+imc = ...
+
+# RESOLUÇÃO DO EXERCÍCIO
+
+imc = peso / (altura ** 2)
+
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}.')
