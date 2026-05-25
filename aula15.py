d51d65229314a32da422acbca7fcf3185a01c0dd
DanielaThais <daniela.thais64@gmail.com>
2026-05-25 16:09:24 -0300

    utilizando input para coleta de dados do usuário

---
+++ aula15.py
@@ -0,0 +1,8 @@
+# nome = input('Qual o seu nome? ')
+# print(f'Seu nome é {nome}')
+
+print("Bem vindo à calculadora")
+numero_1 = input('Digite o primeiro número: ')
+numero_2 = input('Digite o segundo número: ')
+
+print(f'O resultado da soma entre {numero_1} e {numero_2} é igual a {int(numero_1) + int(numero_2)}')
