32498b69fef92e90c863366588dd40fc777a40a6
DanielaThais <daniela.thais64@gmail.com>
2026-05-25 16:18:01 -0300

    introdução aos condicionais

---
+++ aula16.py
@@ -0,0 +1,18 @@
+# OPERAÇÕES CONDICIONAIS
+
+# if / elif / else
+# se / se não se / se não
+
+entrada = input ('Você quer "entrar" ou "sair"?')
+
+if entrada == 'entrar':
+    print('\nVocê entrou no sistema!')
+
+    print(12341234)
+
+elif entrada == 'sair':
+    print('\nVocê saiu do sistema!')
+else:
+    print('\nVocê não digitou nem "entrar" nem "sair".')
+
+print ('\nFim do programa.')
