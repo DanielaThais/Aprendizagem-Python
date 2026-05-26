074a4f71c839ae5b635ac1b8d7f87b87ef20213f
DanielaThais <daniela.thais64@gmail.com>
2026-05-25 23:28:27 -0300

    mais sobre condições if, elif, else

---
+++ aula17.py
@@ -0,0 +1,22 @@
+# OPERAÇÕES CONDICIONAIS
+
+# if / elif / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1:
+    print ('Codigo para condição 1')
+elif condicao2:
+    print ('Codigo para condição 2')
+elif condicao3:
+    print ('Codigo para condição 3')
+elif condicao4:
+    print ('Codigo para condição 4')
+else:
+    print ('Nenhuma condição satisfeita')
+    
+print('\nCódigo fora do bloco...')
