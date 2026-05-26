eb7110362f16af1d45954a034204bac50fdd9ca1
DanielaThais <daniela.thais64@gmail.com>
2026-05-25 23:39:41 -0300

    aprendendo a usa ro debugger no vscode

---
+++ aula18.py
@@ -0,0 +1,22 @@
+# APRENDENDO A USAR O DEBUGGER DO PYTHON NO VSCODE
+
+# if / elif / else
+# se / se não se / se não
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print ('Codigo para condição 1')
+elif condicao2:
+    print ('Codigo para condição 2')
+elif condicao3:
+    print ('Codigo para condição 3')
+elif condicao4:
+    print ('Codigo para condição 4')
+else:
+    print ('Nenhuma condição satisfeita')
+    
+print('\nCódigo fora do bloco...')
