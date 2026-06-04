7c76b07e6409f14e5aee4487bba28f8756c5bf7a
DanielaThais <daniela.thais64@gmail.com>
2026-06-04 20:10:29 -0300

    identidade, valores e memória

---
+++ aula31.py
@@ -0,0 +1,20 @@
+"""
+IDENTIDADE, VALORES E MEMÓRIA 
+
+Flag (Bandiera) - marcar um local
+None = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade (endereço de memória)
+"""
+
+v1 = 'a'
+v2 = 'a'
+print(id(v1))
+print(id(v2))
+
+# condicao = False
+
+# if conficao:
+#   print('Faça algo')
+# else:
+#     print('Não faça algo')
